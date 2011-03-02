741a86a752dd4dc9af16c98d261d0abdccb6c43a
Ali Rantakari <visit-with-web-browser@hasseg.org>
2011-03-03 01:34:11 +0200

    Add Doxygen main page.

---
+++ doxygen.h
@@ -0,0 +1,9 @@
+/** \mainpage
+* 
+* For a fairly automatic solution for syntax highlighting an NSTextField in a Cocoa
+* application, see HGMarkdownHighlighter.
+* 
+* For parsing a Markdown document for syntax highlighting purposes in any C program,
+* see markdown_to_elements().
+* 
+*/
