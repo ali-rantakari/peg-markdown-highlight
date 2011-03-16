4cb02a11a6de75e0f7709043aa922d20e35d4ba5
Ali Rantakari <visit-with-web-browser@hasseg.org>
2011-03-16 13:38:41 +0200

    Remove doxygen doc-string from parse_markdown().
    It's not processed, and it's wrong.

--- markdown_parser_foot.c
+++ markdown_parser_foot.c
@@ -1,16 +1,5 @@
 
 
-/**
-* Parse a Markdown document.
-* 
-* string -- The text to parse
-* elem -- Linked list of RAW or EXTRA_TEXT elements, the former
-*         specifying offset spans in `string` to parse, and the
-*         latter containing additional text to parse but not
-*         take into account when adjusting offsets to match the
-*         original input (`string`).
-* extensions -- A bitfield specifying which extensions to use
-*/
 void parse_markdown(parser_data *p_data)
 {
     MKD_PRINTF("\nPARSER: ");
