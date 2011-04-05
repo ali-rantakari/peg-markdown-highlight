ae0a589251434cd34b7aef33210db5482a98389f
Ali Rantakari <visit-with-web-browser@hasseg.org>
2011-04-06 01:49:35 +0300

    Fix term in docs: view, not field.

--- doxygen/doxygen.h
+++ doxygen/doxygen.h
@@ -1,6 +1,6 @@
 /** \mainpage
 * 
-* For a fairly automatic solution for syntax highlighting an NSTextField in a Cocoa
+* For a fairly automatic solution for syntax highlighting an NSTextView in a Cocoa
 * application, see HGMarkdownHighlighter.
 * 
 * For parsing a Markdown document for syntax highlighting purposes in any C program,
