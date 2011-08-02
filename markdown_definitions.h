41932106f6b0c1b66827be99ef749fb48f9707a1
Ali Rantakari <visit-with-web-browser@hasseg.org>
2011-08-03 02:10:20 +0300

    Tabs to spaces in markdown_definitions.h.

--- markdown_definitions.h
+++ markdown_definitions.h
@@ -25,46 +25,46 @@
 */
 typedef enum
 {
-	LINK,				/**< Link */
-	AUTO_LINK_URL,		/**< Automatic URL link */
-	AUTO_LINK_EMAIL,	/**< Automatic email link */
-	IMAGE,				/**< Image definition */
-	CODE,				/**< Code */
-	HTML,				/**< HTML */
-	HTML_ENTITY,		/**< HTML special entity definition */
-	EMPH,				/**< Emphasized text */
-	STRONG,				/**< Strong text */
-	LIST_BULLET,		/**< Bullet for a list item */
-	LIST_ENUMERATOR,	/**< Enumerator for a list item */
-	COMMENT,			/**< (HTML) comment */
-	
-	/* Code assumes that H1-6 are in order. */
-	H1,					/**< Header, level 1 */
-	H2,					/**< Header, level 2 */
-	H3,					/**< Header, level 3 */
-	H4,					/**< Header, level 4 */
-	H5,					/**< Header, level 5 */
-	H6,					/**< Header, level 6 */
-	
-	BLOCKQUOTE,			/**< Blockquote */
-	VERBATIM,			/**< Verbatim */
-	HTMLBLOCK,			/**< .. */
-	HRULE,				/**< Horizontal rule */
-	REFERENCE,			/**< Reference */
-	NOTE,				/**< Note */
-	
-	/* Utility types used by the parser itself: */
-	RAW_LIST,	/* List of RAW element lists, each to be processed separately from others
-				 * (for each element in linked lists of this type, `children` points
-				 * to a linked list of RAW elements) */
-	RAW,		/* Span marker for positions in original input to be post-processed
-				 * in a second parsing step */
-	EXTRA_TEXT, /* Additional text to be parsed along with spans in the original input
-				 * (these may be added to linked lists of RAW elements) */
-	SEPARATOR,	/* Separates linked lists of RAW elements into parts to be processed
-				 * separate from each other */
-	NO_TYPE,	/* Placeholder element used while parsing */
-	ALL			/* Linked list of *all* elements created while parsing */
+    LINK,               /**< Link */
+    AUTO_LINK_URL,      /**< Automatic URL link */
+    AUTO_LINK_EMAIL,    /**< Automatic email link */
+    IMAGE,              /**< Image definition */
+    CODE,               /**< Code */
+    HTML,               /**< HTML */
+    HTML_ENTITY,        /**< HTML special entity definition */
+    EMPH,               /**< Emphasized text */
+    STRONG,             /**< Strong text */
+    LIST_BULLET,        /**< Bullet for a list item */
+    LIST_ENUMERATOR,    /**< Enumerator for a list item */
+    COMMENT,            /**< (HTML) comment */
+    
+    /* Code assumes that H1-6 are in order. */
+    H1,                 /**< Header, level 1 */
+    H2,                 /**< Header, level 2 */
+    H3,                 /**< Header, level 3 */
+    H4,                 /**< Header, level 4 */
+    H5,                 /**< Header, level 5 */
+    H6,                 /**< Header, level 6 */
+    
+    BLOCKQUOTE,         /**< Blockquote */
+    VERBATIM,           /**< Verbatim */
+    HTMLBLOCK,          /**< .. */
+    HRULE,              /**< Horizontal rule */
+    REFERENCE,          /**< Reference */
+    NOTE,               /**< Note */
+    
+    /* Utility types used by the parser itself: */
+    RAW_LIST,   /* List of RAW element lists, each to be processed separately from others
+                 * (for each element in linked lists of this type, `children` points
+                 * to a linked list of RAW elements) */
+    RAW,        /* Span marker for positions in original input to be post-processed
+                 * in a second parsing step */
+    EXTRA_TEXT, /* Additional text to be parsed along with spans in the original input
+                 * (these may be added to linked lists of RAW elements) */
+    SEPARATOR,  /* Separates linked lists of RAW elements into parts to be processed
+                 * separate from each other */
+    NO_TYPE,    /* Placeholder element used while parsing */
+    ALL         /* Linked list of *all* elements created while parsing */
 } element_type;
 
 /**
@@ -84,16 +84,16 @@ typedef enum
 */
 struct Element
 {
-	element_type type;			  /**< \brief type of element */
-	unsigned long pos;			  /**< \brief start offset in input */
-	unsigned long end;			  /**< \brief end offset in input */
-	struct Element *next;		  /**< \brief next element in list */
-	struct Element *allElemsNext; /**< \brief next element in list of all elements */
-	int textOffset;               /**< \brief offset to text (for elements of type EXTRA_TEXT, used when the parser reads the value of 'text') */
-	char *text;					  /**< \brief text content (for elements of type EXTRA_TEXT) */
-	char *label;				  /**< \brief label (for links and references) */
-	char *address;                /**< \brief address (for links and references) */
-	struct Element *children;	  /**< \brief children of element (for elements of type RAW_LIST) */
+    element_type type;            /**< \brief type of element */
+    unsigned long pos;            /**< \brief start offset in input */
+    unsigned long end;            /**< \brief end offset in input */
+    struct Element *next;         /**< \brief next element in list */
+    struct Element *allElemsNext; /**< \brief next element in list of all elements */
+    int textOffset;               /**< \brief offset to text (for elements of type EXTRA_TEXT, used when the parser reads the value of 'text') */
+    char *text;                   /**< \brief text content (for elements of type EXTRA_TEXT) */
+    char *label;                  /**< \brief label (for links and references) */
+    char *address;                /**< \brief address (for links and references) */
+    struct Element *children;     /**< \brief children of element (for elements of type RAW_LIST) */
 };
 typedef struct Element element;
 
@@ -103,7 +103,7 @@ typedef struct Element element;
 enum markdown_extensions
 {
     EXT_NONE    = 0,        /**< No extensions */
-	EXT_NOTES   = (1 << 0)  /**< A footnote syntax like that of Pandoc or PHP Markdown Extra */
+    EXT_NOTES   = (1 << 0)  /**< A footnote syntax like that of Pandoc or PHP Markdown Extra */
 };
 
 #endif
