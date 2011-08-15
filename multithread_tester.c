0e78e67f819b8960c2c866a13c8e5e105d5a808b
Ali Rantakari <visit-with-web-browser@hasseg.org>
2011-08-16 01:25:42 +0300

    multithread_tester: compare iteration results.
    Compare the parsing results (as strings, for
    clear presentation of errors) between iterations
    of a thread to make sure that the multithreading
    environment does not cause results to be invalid.

--- multithread_tester.c
+++ multithread_tester.c
@@ -39,6 +39,7 @@ char *get_contents(FILE *f)
 typedef struct
 {
     char *md_content;
+    char *results_str;
     char *name;
     int iterations;
 } threadinfo;
@@ -49,6 +50,7 @@ threadinfo *mk_threadinfo(char *content, char *name, int iterations)
     t->md_content = content;
     t->name = name;
     t->iterations = iterations;
+    t->results_str = NULL;
     return t;
 }
 
@@ -61,6 +63,41 @@ double get_time()
 }
 
 
+char *elements_to_str(pmh_element **elems)
+{
+    char *out = malloc(sizeof(char));
+    out[0] = '\0';
+    size_t content_len = 1;
+    
+    int i;
+    for (i = 0; i < pmh_NUM_LANG_TYPES; i++)
+    {
+        char *f;
+        content_len += asprintf(&f, "%i:", i);
+        out = realloc(out, content_len);
+        strcat(out, f);
+        free(f);
+        
+        pmh_element *cursor = elems[i];
+        while (cursor != NULL)
+        {
+            char *ff;
+            content_len += asprintf(&ff, "%ld-%ld,", cursor->pos, cursor->end);
+            out = realloc(out, content_len);
+            strcat(out, ff);
+            free(ff);
+            cursor = cursor->next;
+        }
+        
+        content_len += 1;
+        out = realloc(out, content_len);
+        strcat(out, "|");
+    }
+    
+    return out;
+}
+
+
 void *thread_run(void *arg)
 {
     threadinfo *ti = (threadinfo *)arg;
@@ -68,10 +105,30 @@ void *thread_run(void *arg)
     for (i = 0; i < ti->iterations; i++)
     {
         int sleep_ms = (int)get_time() % 100;
-        fprintf(stderr, "%s (iteration %i/%i, sleep %i ms after)\n", ti->name, i+1, ti->iterations, sleep_ms);
+        fprintf(stderr, "%s (iteration %i/%i, sleep %i ms after)\n",
+                ti->name, i+1, ti->iterations, sleep_ms);
+        
         pmh_element **result;
         pmh_markdown_to_elements(ti->md_content, pmh_EXT_NONE, &result);
+        pmh_sort_elements_by_pos(result);
+        
+        char *this_results = elements_to_str(result);
+        if (ti->results_str == NULL)
+            ti->results_str = this_results;
+        else
+        {
+            if (strcmp(this_results, ti->results_str) != 0)
+            {
+                fprintf(stderr, "ERROR! Thread %s results differ:\n\n"
+                                "1st iteration results:\n%s\n"
+                                "this iteration (#%i) results:\n%s\n",
+                                ti->name, ti->results_str, i+1, this_results);
+            }
+            free(this_results);
+        }
+        
         pmh_free_elements(result);
+        
         usleep(sleep_ms);
     }
     return NULL;
@@ -93,13 +150,17 @@ int main(int argc, char *argv[])
     char **md_sources = NULL;
     int *md_source_lengths = NULL;
     int num_md_sources = 0;
-    for (i=0, j=0; i < argc; i++) {
-        if (strcmp(argv[i], "--") == 0) {
+    for (i=0, j=0; i < argc; i++)
+    {
+        if (strcmp(argv[i], "--") == 0)
+        {
             num_md_sources = argc-i-1;
             md_sources = (char **)malloc(sizeof(char*)*num_md_sources);
             md_source_filenames = (char **)malloc(sizeof(char*)*num_md_sources);
             md_source_lengths = (int *)malloc(sizeof(int*)*num_md_sources);
-        } else if (md_sources != NULL) {
+        }
+        else if (md_sources != NULL)
+        {
             printf("read %s\n", argv[i]);
             md_source_filenames[j] = argv[i];
             FILE *f = fopen(argv[i], "r");
@@ -111,23 +172,28 @@ int main(int argc, char *argv[])
     }
     
     if (md_sources == NULL) {
-        printf("usage: %s [NUM_THREADS] [NUM_ITERATIONS] -- file1 [file2...]\n", basename(argv[0]));
+        printf("usage: %s [NUM_THREADS] [NUM_ITERATIONS] -- file1 [file2...]\n",
+               basename(argv[0]));
         return 0;
     }
     
-    printf("Parsing input in %i threads, %i iterations each.\n", num_threads, num_iterations);
+    printf("Parsing input in %i threads, %i iterations each.\n",
+           num_threads, num_iterations);
     
     int colors[5] = {31,32,33,35,36};
     int num_colors = 5;
     
     pthread_t threads[num_threads];
-    for (i=0, j=0, k=0; i < num_threads; i++, j = ((j+1) % num_md_sources), k = ((k+1) % num_colors))
+    for (i=0, j=0, k=0;
+         i < num_threads;
+         i++, j = ((j+1) % num_md_sources), k = ((k+1) % num_colors))
     {
         char *md_source_copy = malloc(md_source_lengths[j]+1);
         strcpy(md_source_copy, md_sources[j]);
         pthread_t thread;
         char *name = malloc(sizeof(char)*(strlen(md_source_filenames[j])+30));
-        sprintf(name, "\e[%imt%i\e[0m \e[34m<%s>\e[0m", colors[k], i+1, md_source_filenames[j]);
+        sprintf(name, "\e[%imt%i\e[0m \e[34m<%s>\e[0m",
+                colors[k], i+1, md_source_filenames[j]);
         threadinfo *ti = mk_threadinfo(md_source_copy, name, num_iterations);
         pthread_create(&thread, NULL, thread_run, (void *)ti);
         threads[i] = thread;
